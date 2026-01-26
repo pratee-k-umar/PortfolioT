54b34fe85303d5dbba9c2303c017fb64936cb14f
Prateek Kumar <88579575+pratee-k-umar@users.noreply.github.com>
2026-01-26 22:15:40 +0530

    Change MongoDB database name from 'user' to 'capx'

--- utils/database.js
+++ utils/database.js
@@ -10,7 +10,7 @@ export const connectToDB = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "user",
+      dbName: "capx",
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
