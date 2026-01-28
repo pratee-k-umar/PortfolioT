c06eec1e0bad9539b0ec90ff3f7f0ea76d5efb70
Prateek Kumar <prateek0426@gmail.com>
2026-01-28 15:12:28 +0530

    resolve next config issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverComponentsExternalPackages: ["mongoose"],
-  images: {
-    domains: ["lh3.googleusercontent.com", "res.cloudinary.com"]
-  },
-  webpack(config) {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true
-    }
-    return config
-  },
-  async server({ httpserver }) {
-    const { WebSocketServer } = require('ws')
-    const wss = new WebSocketServer({ server: httpserver })
-    wss.on('connection', (ws) => {
-      console.log("New websocket connection established...")
-      ws.on("message", (message) => {
-        console.log("Message recieved...")
-        ws.send(`Server echo: ${message}`)
-      })
-      ws.on("close", () => {
-        console.log("Connection closed...")
-      })
-    })
-    console.log("WebSocket server started...")
-  }
-}
+    images: {
+        domains: ["lh3.googleusercontent.com", "res.cloudinary.com"],
+    },
+    webpack(config) {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        };
+        return config;
+    },
+};
 
-export default nextConfig
+export default nextConfig;
